7a98181e0773727d651e578750906f24735d6ddf
Tyler Gary <tygary@gmail.com>
2020-03-23 11:09:35 -0700

    Adding modulo and fixing semicolons

--- global.js
+++ global.js
@@ -8,19 +8,23 @@ console.log(4 + 5)
 
 //1.) Log an expression that evaluates to the sum of two integers
 // your code here 
-console.log('answer 1')
+console.log('answer 1');
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here 
-console.log('answer 2')
+console.log('answer 2');
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here 
-console.log('answer 3')
+console.log('answer 3');
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here 
-console.log('answer 4')
+console.log('answer 4');
+
+// 5.) Log an expression that uses modulo, with a result of 4
+// your code here
+console.log('answer 5');
 
 /**********************/
 /* Part 2: Statements */
@@ -28,11 +32,11 @@ console.log('answer 4')
 
 // 5.) Assign a variable and console log the declaration
 // your code here 
-console.log('answer 5')
+console.log('answer 5');
 
 // 6.) Console log a variable assigned to the sum of two numbers
 // your code here
-console.log('answer 6')
+console.log('answer 6');
 
 // 7.) Uncomment this if else statement and add a logical expression as the condition
 //if (/*your code here*/) {
